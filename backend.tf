0de76db13e605530e11b7f1c44512115ee9c6c51
atiframzan89 <atif.ramzan89@gmail.com>
2023-03-23 12:05:19 +0500

    adding backend changes for devel branch

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "fullstack-labs-dev"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "fullstack-labs-tf"
+    profile        = "aramzan"
+  }
+}
